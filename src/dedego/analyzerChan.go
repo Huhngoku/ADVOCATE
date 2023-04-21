32d194f9930c38bef0664265cf9f7b31b11480f8
Erik Kassubek <53377037+ErikKassubek@users.noreply.github.com>
2023-04-21 16:06:18 +0200

    t1

--- src/dedego/analyzerChan.go
+++ src/dedego/analyzerChan.go
@@ -687,9 +687,9 @@ func update_receive(vectorClock [][]int, routineRec int, routineSend int, vector
 
 	c[routineRec][routineRec]++
 
-	if c[routineRec][routineRec] <= vectorClockSender[routineRec] {
-		c[routineRec][routineRec] = vectorClockSender[routineRec] + 1
-	}
+	// if c[routineRec][routineRec] <= vectorClockSender[routineRec] {
+	// 	c[routineRec][routineRec] = vectorClockSender[routineRec] + 1
+	// }
 
 	for l := 0; l < len(c[routineRec]); l++ {
 		if c[routineRec][l] < vectorClockSender[l] {
