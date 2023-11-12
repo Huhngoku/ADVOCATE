d0209b51e358effb43d14a1d2d0e3349b7baadbf
Erik Kassubek <kassubek.erik@gmail.com>
2023-11-12 12:40:14 +0100

    update constructed

--- examples/sendToClosedChan/constructed/sendToClosedChan.go
+++ examples/sendToClosedChan/constructed/sendToClosedChan.go
@@ -441,36 +441,7 @@ const N = 20
 func main() {
 
 	runtime.InitCobufi(0)
-	defer cobufi.CreateTrace("constructedTest.log")
-
-	// defer func() {
-	// 	runtime.DisableTrace()
-
-	// 	file_name := "constructed.log"
-	// 	os.Remove(file_name)
-	// 	file, err := os.OpenFile(file_name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	defer file.Close()
-
-	// 	numRout := runtime.GetNumberOfRoutines()
-	// 	for i := 1; i <= numRout; i++ {
-	// 		cobufiChan := make(chan string)
-	// 		go func() {
-	// 			runtime.TraceToStringByIdChannel(i, cobufiChan)
-	// 			close(cobufiChan)
-	// 		}()
-	// 		for trace := range cobufiChan {
-	// 			if _, err := file.WriteString(trace); err != nil {
-	// 				panic(err)
-	// 			}
-	// 		}
-	// 		if _, err := file.WriteString("\n"); err != nil {
-	// 			panic(err)
-	// 		}
-	// 	}
-	// }()
+	defer cobufi.CreateTrace("constructed.log")
 
 	ns := [N]func(){n01, n02, n03, n04, n05, n06, n07, n08, n09, n10, n11, n12,
 		n13, n14, n15, n16, n17, n18, n19, n20}
@@ -479,59 +450,3 @@ func main() {
 		ns[i]()
 	}
 }
-
-/*
-==================== Summary ====================
-
--------------------- Critical -------------------
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:129
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:121
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:143
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:138
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:180
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:186
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:181
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:196
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:313
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:301
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:360
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:350
-Possible send on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:436
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:428
--------------------- Warning --------------------
-Found concurrent Send on same channel:
-	send: /home/erik/Uni/HiWi/CoBuFi-Go/go-patch/src/runtime/mgcsweep.go:279
-	send : /home/erik/Uni/HiWi/CoBuFi-Go/go-patch/src/runtime/mgcscavenge.go:652
-Possible receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:129
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:125
-Possible receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:143
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:139
-Receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:168
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:165
-Possible receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:180
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:197
-Possible receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:181
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:191
-Receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:339
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:333
-Possible receive on closed channel:
-	close: /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:360
-	recv : /home/erik/Uni/HiWi/CoBuFi-Go/examples/sendToClosedChan/constructed/sendToClosedChan.go:356
-
-=================================================
-Total runtime: 9.958979ms
-=================================================
-*/
