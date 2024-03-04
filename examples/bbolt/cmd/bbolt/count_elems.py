1c3fa97893ae8e7da49053d9f6ffe349c24af77d
ErikKassubek <kassubek.erik@gmail.com>
2024-03-04 11:03:36 +0100

    add example programs

---
+++ examples/bbolt/cmd/bbolt/count_elems.py
@@ -0,0 +1,9 @@
+elems = 0
+lines = 0
+with open('trace.log', 'r') as file:
+    for line in file:
+        lines += 1
+        if len(line) == 0:
+            continue
+        elems += (line.count(';') + 1)
+print(lines, elems)
---
+++ examples/sorty/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"advocate"
+	"math/rand"
+)
+
+func main() {
+	if false {
+		// init tracing
+		advocate.InitTracing(0)
+		defer advocate.Finish()
+	} else {
+		// init replay
+		advocate.EnableReplay()
+		defer advocate.WaitForReplayFinish()
+	}
+
+	l := 100000
+	input := make([]int, l)
+	rand.Seed(1) // added to create same sequence in replay
+	for i := 0; i < l; i++ {
+		input[i] = rand.Intn(l) + 1
+	}
+	SortSlice(input)
+	_ = input
+}
