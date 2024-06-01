2e2c58bb2f08690b49ae582fead178e705c207a9
yellow-tshirt <mar.occhinegro@gmail.com>
2024-06-01 14:57:05 +0200

    added 56

---
+++ examples/canonicalTests/tests/56_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestSomething(t *testing.T) {
+	n56()
+}
+
+// leak because of conditional variable
+func n56() {
+	c := sync.NewCond(&sync.Mutex{})
+
+	// wait for signal
+	go func() {
+		c.L.Lock()
+		c.Wait()
+		c.L.Unlock()
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+}
