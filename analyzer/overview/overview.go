6c2ede16d97f296ae0992526a255dc374cab68da
ErikKassubek <kassubek.erik@gmail.com>
2024-06-14 14:02:09 +0200

    start with overview for bug

---
+++ analyzer/overview/overview.go
@@ -0,0 +1,8 @@
+package overview
+
+// create an overview over an analyzed, and if possible replayed
+// bug. It is mostly meant to give an explanation of a found
+// bug to people, who are not used to the internal structure an
+// representation of the analyzer.
+
+// It creates one file. This file has the following form:
