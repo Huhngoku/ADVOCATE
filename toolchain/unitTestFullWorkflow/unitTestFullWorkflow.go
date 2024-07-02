b4b50843461bc9b2db02950071116f50119f6fc5
yellow-tshirt <mar.occhinegro@gmail.com>
2024-07-02 21:37:37 +0200

    put some functionality into functions

--- toolchain/unitTestFullWorkflow/unitTestFullWorkflow.go
+++ toolchain/unitTestFullWorkflow/unitTestFullWorkflow.go
@@ -36,59 +36,68 @@ func main() {
 	pathToGoRoot := filepath.Join(*advocateRoot, "go-patch")
 	pathToOverheadInserter := filepath.Join(*advocateRoot, "toolchain", "unitTestOverheadInserter", "unitTestOverheadInserter")
 	pathToOverheadRemover := filepath.Join(*advocateRoot, "toolchain", "unitTestOverheadRemover", "unitTestOverheadRemover")
+	//initial setup
+	initialSetup(projectRoot, pathToGoRoot, testFile, testName)
+	fmt.Println("Remove Overhead just in case")
+	removeOverhead(pathToOverheadRemover, testFile, testName)
+	fmt.Println("Add Overhead")
+	addOverhead(pathToOverheadInserter, testFile, testName)
+	fmt.Println("Run test")
+	runTest(moduleMode, pathToPatchedGoRuntime, nil, testFile, testName, pathToOverheadRemover)
 
-	//change directory to the project root folder
+}
+func initialSetup(projectRoot *string, pathToGoRoot string, testFile *string, testName *string) {
 	err := os.Chdir(*projectRoot)
 	if err != nil {
 		fmt.Println("Error changing directory:", err)
 		return
 	}
 	fmt.Println("In directory:", *projectRoot)
-	//export GOROOT=$advocateRoot/go-patch
 	err = os.Setenv("GOROOT", pathToGoRoot)
 	if err != nil {
 		fmt.Println("Error setting GOROOT:", err)
 		return
 	}
 	fmt.Println("GOROOT set to:", pathToGoRoot)
-	//create file called advocateCommand.log
 	advocateCommandLog, err := os.Create("advocateCommand.log")
 	if err != nil {
 		fmt.Println("Error creating file:", err)
 		return
 	}
-	//append filepath to command log
 	_, err = advocateCommandLog.WriteString(*testFile + "\n")
-	//append testname to commad log
 	_, err = advocateCommandLog.WriteString(*testName + "\n")
-	//append module mode to command log
-	fmt.Println("Remove Overhead just in case")
-	//execute overhead remover: Usage $pathToOverheadRemover -f $file -t $testName
-	cmd := exec.Command(pathToOverheadRemover, "-f", *testFile, "-t", *testName)
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println("Error removing overhead:", err)
-		return
-	}
+}
+func addOverhead(pathToOverheadInserter string, testFile *string, testName *string) {
 	//Add overhead: Usage $pathToOverheadInserter -f $file -t $testName
 	fmt.Println("Add Overhead")
-	cmd = exec.Command(pathToOverheadInserter, "-f", *testFile, "-t", *testName)
-	err = cmd.Run()
+	cmd := exec.Command(pathToOverheadInserter, "-f", *testFile, "-t", *testName)
+	err := cmd.Run()
 	if err != nil {
 		fmt.Println("Error adding overhead:", err)
 		return
 	}
-	fmt.Println("Run test")
+}
+func removeOverhead(pathToOverheadRemover string, testFile *string, testName *string) {
+	//Remove overhead: Usage $pathToOverheadRemover -f $file -t $testName
+	fmt.Println("Remove Overhead")
+	cmd := exec.Command(pathToOverheadRemover, "-f", *testFile, "-t", *testName)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error removing overhead:", err)
+		return
+	}
+}
+func runTest(moduleMode bool, pathToPatchedGoRuntime string, advocateCommandLog *os.File, testFile *string, testName *string, pathToOverheadRemover string) {
 	//check for module mode
 	if moduleMode {
 		// echo "$pathToPatchedGoRuntime test -count=1 -run=$testName -mod=mod ./$package" >>advocateCommand.log
 		// $pathToPatchedGoRuntime test -count=1 -run=$testName -mod=mod "./$package"
-		_, err = advocateCommandLog.WriteString(pathToPatchedGoRuntime + " test -count=1 -run=" + *testName + " -mod=mod ./" + *testFile + "\n")
+		_, err := advocateCommandLog.WriteString(pathToPatchedGoRuntime + " test -count=1 -run=" + *testName + " -mod=mod ./" + *testFile + "\n")
 		if err != nil {
 			fmt.Println("Error writing to file:", err)
 			return
 		}
-		cmd = exec.Command(pathToPatchedGoRuntime, "test", "-count=1", "-run="+*testName, "-mod=mod", "./"+*testFile)
+		cmd := exec.Command(pathToPatchedGoRuntime, "test", "-count=1", "-run="+*testName, "-mod=mod", "./"+*testFile)
 		err = cmd.Run()
 		if err != nil {
 			fmt.Println("Remove Overhead")
@@ -98,12 +107,12 @@ func main() {
 		}
 	} else {
 		//do the same without -mod=mod
-		_, err = advocateCommandLog.WriteString(pathToPatchedGoRuntime + " test -count=1 -run=" + *testName + " ./" + *testFile + "\n")
+		_, err := advocateCommandLog.WriteString(pathToPatchedGoRuntime + " test -count=1 -run=" + *testName + " ./" + *testFile + "\n")
 		if err != nil {
 			fmt.Println("Error writing to file:", err)
 			return
 		}
-		cmd = exec.Command(pathToPatchedGoRuntime, "test", "-count=1", "-run="+*testName, "./"+*testFile)
+		cmd := exec.Command(pathToPatchedGoRuntime, "test", "-count=1", "-run="+*testName, "./"+*testFile)
 		err = cmd.Run()
 		if err != nil {
 			fmt.Println("Remove Overhead")
@@ -112,35 +121,5 @@ func main() {
 			return
 		}
 	}
-	// echo "Remove Overhead"
-	// echo "$pathToOverheadRemover -f $file -t $testName" >>advocateCommand.log
-	// $pathToOverheadRemover -f $file -t $testName
-	// echo "$pathToAnalyzer -t $dir/$package/advocateTrace" >>advocateCommand.log
-	// $pathToAnalyzer -t "$dir/$package/advocateTrace"
-	// rewritten_traces=$(find "./$package" -type d -name "rewritten_trace*")
-	// for trace in $rewritten_traces; do
-	// 	rtracenum=$(echo $trace | grep -o '[0-9]*$')
-	// 	echo "Apply reorder overhead"
-	// 	echo $pathToOverheadInserter -f $file -t $testName -r true -n "$rtracenum" >>advocateCommand.log
-	// 	$pathToOverheadInserter -f $file -t $testName -r true -n "$rtracenum"  >>advocateCommand.log
-	// 	if [ "$modulemode" == "true" ]; then
-	// 		echo "$pathToPatchedGoRuntime test -count=1 -run=$testName -mod=mod ./$package" >>advocateCommand.log
-	// 		$pathToPatchedGoRuntime test -count=1 -run=$testName -mod=mod "./$package" 2>&1 | tee -a "$trace/reorder_output.txt"
-	// 	else
-	// 		echo "$pathToPatchedGoRuntime test -count=1 -run=$testName ./$package" >>advocateCommand.log
-	// 		$pathToPatchedGoRuntime test -count=1 -run=$testName "./$package" 2>&1 | tee -a "$trace/reorder_output.txt"
-	// 	fi
-	// 	echo "Remove reorder overhead"
-	// 	echo "$pathToOverheadRemover -f $file -t $testName" >>advocateCommand.log
-	// 	$pathToOverheadRemover -f $file -t $testName
-	// done
-	fmt.Println("Remove Overhead")
-	_, err = advocateCommandLog.WriteString(pathToOverheadRemover + " -f " + *testFile + " -t " + *testName + "\n")
-	cmd = exec.Command(pathToOverheadRemover, "-f", *testFile, "-t", *testName)
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println("Error removing overhead:", err)
-		return
-	}
-	_, err 
+
 }
